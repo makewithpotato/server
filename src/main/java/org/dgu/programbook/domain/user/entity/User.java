ca5cae723d5e8d681b3a3970b38b7239d38952ba
hysong4u <hayeon_song@naver.com>
2025-06-17 19:26:23 +0900

    [feat] #15 유저 기본 필드 추가

--- src/main/java/org/dgu/programbook/domain/user/entity/User.java
+++ src/main/java/org/dgu/programbook/domain/user/entity/User.java
@@ -1,15 +1,28 @@
 package org.dgu.programbook.domain.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
+@AllArgsConstructor
 @Getter
 @NoArgsConstructor
 @Table(name = "user")
 @Entity
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String socialId;
+
+    private String imageUrl;
+
+    private String email;
+
 }
