67740175e8416e262fa036cb6ed0d00f161b36f3
hysong4u <hayeon_song@naver.com>
2025-12-04 04:26:39 +0900

    [fix] AI서버에서 받아오는 DTO 수정

--- src/main/java/org/dgu/programbook/domain/movie/dto/response/AnalysisResponse.java
+++ src/main/java/org/dgu/programbook/domain/movie/dto/response/AnalysisResponse.java
@@ -3,10 +3,11 @@ package org.dgu.programbook.domain.movie.dto.response;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class AnalysisResponse {
-    private List<String> prompt2results;
-    private List<RetrievalResult> retrieval2uris;
+    private List<List<String>> prompt2results;
+    private Map<String, List<String>> retrieval2uris;
     private String thumbnail_folder_uri;
 }
